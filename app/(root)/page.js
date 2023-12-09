7f7baa86c79d322e9590378563ecc50ba1878751
Darshan Rajpurohit <darshanrajpurohit47@gmail.com>
2023-12-10 01:13:21 +0530

    first commit

---
+++ app/(root)/page.js
@@ -0,0 +1,12 @@
+import CreateContract from "@/components/common/CreateContract";
+
+async function Home() {
+
+  return (
+    <main className="">
+      <CreateContract />
+    </main>
+  );
+}
+
+export default Home;
